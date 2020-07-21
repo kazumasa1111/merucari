58c68b7df6f07a0a6f4ffe4f71cb7d1790bccfff
kanasakakei <soraito6254@yahoo.co.jp>
2020-07-22 00:10:59 +0900

    モデル作成　マイグレーション

---
+++ db/migrate/20200721142817_create_products.rb
@@ -0,0 +1,19 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+
+      t.string :name, null:false
+      t.text   :description_of_item , null:false
+      t.string :commodity_condition , null:false
+      t.string :shipping_charges	  , null:false
+      t.string :days_until_shipping	, null:false
+      t.string :price               , null:false
+
+      # t.references :user    , null:false, foreign_key: true
+      # t.references :category, null:false, foreign_key: true
+      # t.references :brand   , null:false, foreign_key: true
+      # t.references :image   , null:false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,44 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_07_21_144305) do
+
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description_of_item", null: false
+    t.string "commodity_condition", null: false
+    t.string "shipping_charges", null: false
+    t.string "days_until_shipping", null: false
+    t.string "price", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
