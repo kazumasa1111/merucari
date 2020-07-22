7d8850cc08d8a6f04aac03e7d38a890a93c6fa46
hideokawahara <kawahide.72@gmail.com>
2020-07-22 23:35:43 +0900

    ユーザー登録ページ作成

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,10 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  def new_address
+  end
+
+  def create_address
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  devise_scope :user do
+    get  'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
   root 'items#index'
   get 'test_products/show'
 end
