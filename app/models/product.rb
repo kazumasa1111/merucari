e504d1a74727a737eddc0a253aba6543e9de1932
kanasakakei <soraito6254@yahoo.co.jp>
2020-08-01 22:13:57 +0900

    モデル　テスト

--- app/models/product.rb
+++ app/models/product.rb
@@ -16,4 +16,8 @@ class Product < ApplicationRecord
 
   validates :name, :description_of_item ,:commodity_condition,
             :shipping_charges,:prefecture_id, :days_until_shipping,:price  ,presence: true
+  
+  validates :name, length: { maximum: 40}
+  validates :description_of_item , length: { maximum: 1000}
+  validates :price, numericality: {only_integer:true, greater_than:299, less_than:10000000}
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,9 +3,76 @@ require 'rails_helper'
 describe Product do
   describe '#create' do
 
-    it "画像以外存在すれば登録できる" do
-            product = build(:product)
-            expect(product).to be_valid
+    it "以外存在すれば登録できる" do
+      product = build(:product)
+      expect(product).to be_valid
     end
+
+    it "商品名がないと登録できない" do
+      product = build(:product, name: nil)
+      product.valid?
+      expect(product.errors[:name]).to include("を入力してください")
+    end
+
+    it "商品説明がないと登録できない" do
+      product = build(:product, description_of_item: nil)
+      product.valid?
+      expect(product.errors[:description_of_item]).to include("を入力してください")
+    end
+
+    it "商品の状態がないと登録できない" do
+      product = build(:product, commodity_condition: nil)
+      product.valid?
+      expect(product.errors[:commodity_condition]).to include("を入力してください")
+    end
+
+    it "配送料の負担がないと登録できない" do
+      product = build(:product, shipping_charges: nil)
+      product.valid?
+      expect(product.errors[:shipping_charges]).to include("を入力してください")
+    end
+
+    it "発送までの日数がないと登録できない" do
+      product = build(:product, days_until_shipping: nil)
+      product.valid?
+      expect(product.errors[:days_until_shipping]).to include("を入力してください")
+    end
+
+    it "発送元の地域がないと登録できない" do
+      product = build(:product, prefecture_id: nil)
+      product.valid?
+      expect(product.errors[:prefecture_id]).to include("を入力してください")
+    end
+
+    it "価格がないと登録できない" do
+      product = build(:product, price: nil)
+      product.valid?
+      expect(product.errors[:price]).to include("を入力してください")
+    end
+
+    it "商品名が40文字以上あると登録できない" do
+      product = build(:product, name: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+      product.valid?
+      expect(product.errors[:name]).to include("は40文字以内で入力してください")
+    end
+
+    it "商品説明が1000文字以上あると登録できない" do
+      product = build(:product, description_of_item: "asasassasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasaasaasasasasasasaaaaaaaaaaaaaaaaaaalalalalallallalalalallallalallalalallalalalalallalalalallalalalalalalallalaallalalalaalalalalalalalalalalalalalaaalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalaalalalalalalalalalalalalalalalalalalalalalalalaalalalalalalalalalalalalallllllallaalallalalalallalalalallalalallalallalallalallalallalaalalalallaalalalalalalalalalalalalalalalalaalalalaalalalalallalalalalalalalalalalalalallaalalalalalalalalalalaalallaallaallalalalalalalalalalalalalalalalalalalalalalalaaaaalalalalalalalalalalalalalalalalalallalalalalalalallalalalalalalalalalallalalalalalalalalalalalalalalalalalalalaaalalalaalalalalalalalalalalalalalalalalalalalalalalalalalalalalalllalalalalaallalallalallalllllllllllllllllllllllllllllllllllllあああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ")
+      product.valid?
+      expect(product.errors[:description_of_item]).to include("は1000文字以内で入力してください")
+    end
+
+    it "価格が300円未満は登録できない" do
+      product = build(:product, price: 100)
+      product.valid?
+      expect(product.errors[:price]).to include("は299より大きい値にしてください")
+    end
+
+    it "価格が10000000円以上は登録できない" do
+      product = build(:product, price: 10000000)
+      product.valid?
+      expect(product.errors[:price]).to include("は10000000より小さい値にしてください")
+    end
+    
   end
 end
