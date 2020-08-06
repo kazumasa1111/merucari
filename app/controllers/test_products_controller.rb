1ca3fcc9c25ededd6d128ca1a67cd758b6e58209
kazumasa1111 <kazumasa_machida5298@icloud.com>
2020-08-06 13:24:30 +0900

    商品

--- app/controllers/test_products_controller.rb
+++ app/controllers/test_products_controller.rb
@@ -1,5 +1,4 @@
 class TestProductsController < ApplicationController
-
   def create
   end
   
--- db/schema.rb
+++ db/schema.rb
@@ -29,20 +29,6 @@ ActiveRecord::Schema.define(version: 2020_07_25_070528) do
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
-  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "ancestry"
-    t.index ["ancestry"], name: "index_categories_on_ancestry"
-  end
-
   create_table "credits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "customer_id", null: false
@@ -99,4 +85,5 @@ ActiveRecord::Schema.define(version: 2020_07_25_070528) do
   add_foreign_key "addresses", "users"
   add_foreign_key "credits", "users"
   add_foreign_key "images", "products"
+
 end
