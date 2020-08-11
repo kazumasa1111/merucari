2f9cbeaf3779644622d194b6bb60ecf58a7ab546
kanasakakei <soraito6254@yahoo.co.jp>
2020-08-11 20:06:11 +0900

    View 修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
   def index
+    @parents = Category.where(ancestry: nil)
   end
+
+  def show
+    @parents = Category.where(ancestry: nil)
+  end
+
 end
