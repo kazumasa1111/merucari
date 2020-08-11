d0d069354e4c1ff18225322f439f97144c30c841
kanasakakei <soraito6254@yahoo.co.jp>
2020-08-11 13:08:53 +0900

    修正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,29 +1,27 @@
 class CategoriesController < ApplicationController
-before_action :set_category, only: :show 
+  before_action :set_category, only: :show 
 
 
 
-def index
-  @parents = Category.all
-end
+  def index
+    @parents = Category.all
+  end
 
 
-def show
-  @parents = Category.where(ancestry: nil)
-  @products = @category.set_products
-  @products = @products.order("created_at DESC")
-end
+  def show
+    @parents = Category.where(ancestry: nil)
+    @products = @category.set_products
+    @products = @products.order("created_at DESC")
+  end
 
 
 
 
-private
+  private
 
-def set_category
-  @category = Category.find(params[:id])
-end
-
-
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -12,14 +12,11 @@ class Category < ApplicationRecord
       start_id = self.indirects.first.id
       end_id = self.indirects.last.id
       products = Product.where(category_id: start_id..end_id)
-    
-      return products
 
     elsif self.has_children?
       start_id = self.children.first.id
       end_id = self.children.last.id
       products = Product.where(category_id: start_id..end_id)
-      return products
     
     else
       return self.products
