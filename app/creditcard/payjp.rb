21efabb3ac2e9bddd22e13d0b30ce7ea1f2f9cb2
kanasakakei <soraito6254@yahoo.co.jp>
2020-09-05 12:13:39 +0900

    Update payjp.rb

--- app/creditcard/payjp.rb
+++ app/creditcard/payjp.rb
@@ -2,7 +2,8 @@ require 'payjp'
 require 'pry'
 
 class MyPayjp
-  Payjp::api_key = ENV["PAYJP_PRIVATE_KEY"]
+  # Payjp::api_key = ENV["PAYJP_PRIVATE_KEY"]
+  Payjp::api_key = 'sk_live_1f91f7629f35be668ff929d25c9161ad7367f736e7068e5b93dd0e3e'
 
   def self.create_token(number, cvc, exp_month, exp_year)
     token = Payjp::Token.create(
